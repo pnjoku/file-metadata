5d4e5c7bbe7e941e379fff7d1fa45c5413d1af6d
Prince Njoku <princenjoku55@gmail.com>
2016-08-27 21:16:09 +0000

    updated all references to file info

--- templates/controller.js
+++ templates/controller.js
@@ -32,7 +32,7 @@ angular
             data: {file: file }
         }).then(function (resp) {
             //console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data)
-            this.fileData = JSON.stringify(resp.data)
+            file.fileData = JSON.stringify(resp.data)
         }, function (resp) {
             //console.log('Error status: ' + resp.status)
         }, function (evt) {
