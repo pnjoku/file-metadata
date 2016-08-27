85cf1a8103d436980783574e02e668942a9b3c7d
Prince Njoku <princenjoku55@gmail.com>
2016-08-27 21:07:04 +0000

    first attempt at getting rid of scope

---
+++ templates/controller.js
@@ -0,0 +1,43 @@
+'use strict'
+angular
+    .module('FileMetadata', ['ngRoute','ngFileUpload'])
+    
+    .config(function($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'index.html',
+            controller: 'fileCtrl', 
+            controllerAs: 'file',
+        })
+    
+    })
+    
+    .controller('fileCtrl', fileController)
+      
+      
+     function fileController($http, Upload){
+         var file = this;
+     this.fileData = "File Data will display here"
+     file.submit = submit;
+     file.upload = upload;
+      
+      function submit() {
+      if (this.file) {
+        this.upload(this.file)
+        }
+      }
+       
+      function upload(file) {
+        Upload.upload({
+            url: '/upload',
+            data: {file: file }
+        }).then(function (resp) {
+            //console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data)
+            this.fileData = JSON.stringify(resp.data)
+        }, function (resp) {
+            //console.log('Error status: ' + resp.status)
+        }, function (evt) {
+            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total)
+            //console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name)
+        })
+      }
+     } 
