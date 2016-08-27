3458efd69776586c10fb4435065f07e6380b5b04
Prince Njoku <princenjoku55@gmail.com>
2016-08-27 17:41:42 +0000

    removed semicolons, using es6 now, added npm start

--- server.js
+++ server.js
@@ -1,86 +1,86 @@
 'use strict'
 
-var express = require('express');
-var path = require('path');
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var log = require('npmlog');
-var fs = require('fs');
-var multer = require('multer');
-var bodyParser = require('body-parser');
+let express = require('express'),
+    path = require('path'),
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    log = require('npmlog'),
+    fs = require('fs'),
+    multer = require('multer'),
+    bodyParser = require('body-parser')
 
 
-var fileSchema = new Schema({
+let fileSchema = new Schema({
   name: String,
   size: Number,
   date: String
-});
+})
 
-var app = express();
+let app = express()
 
-var File = mongoose.model('File', fileSchema);
-var mongouri = process.env.MONGOLAB_URI || "mongodb://localhost:27017/img-sal";
-mongoose.Promise = global.Promise;
-mongoose.connect(mongouri);
+let File = mongoose.model('File', fileSchema)
+let mongouri = process.env.MONGOLAB_URI || "mongodb://localhost:27017/metadata"
+mongoose.Promise = global.Promise
+mongoose.connect(mongouri)
 
-app.use(express.static(path.join(__dirname, 'templates')));
-app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'templates')))
+app.use(bodyParser.json())
 app.use(multer({
   dest: path.join(__dirname, '/uploads/')
-}).any());
+}).any())
 
-var storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, 'uploads/');
+let storage = multer.diskStorage({
+  destination: (req, file, cb) =>{
+    cb(null, 'uploads/')
   },
-  filename: function(req, file, cb) {
-    var getFileExt = function(fileName) {
-      var fileExt = fileName.split(".");
+  filename: (req, file, cb) =>{
+    let getFileExt = (fileName) => {
+      let fileExt = fileName.split(".")
       if (fileExt.length === 1 || (fileExt[0] === "" && fileExt.length === 2)) {
-        return "";
+        return ""
       }
-      return fileExt.pop();
-    };
-    cb(null, Date.now() + '.' + getFileExt(file.originalname));
+      return fileExt.pop()
+    }
+    cb(null, Date.now() + '.' + getFileExt(file.originalname))
   }
-});
+})
 
-var multerUpload = multer({
+let multerUpload = multer({
   storage: storage
 });
-var uploadFile = multerUpload.single('userFile');
+let uploadFile = multerUpload.single('userFile')
 
-app.post('/upload', function(req, res) {
+app.post('/upload', (req, res) => {
     uploadFile(req, res, function(err) {
       if (err) {
-        log.error(err);
+        log.error(err)
       }
-      var fileDetails = {
+      let fileDetails = {
         name: req.files[0].originalname,
         size: req.files[0].size,
         date: new Date().toLocaleString()
-      };
+      }
 
-      var file = new File(fileDetails);
-      file.save(function(err, file) {
+      let file = new File(fileDetails)
+      file.save((err, file) => {
         if (err) {
-          log.error(err);
-          throw err;
+          log.error(err)
+          throw err
         }
-        log.info('Saved', file);
-      });
-      var filePath = "./uploads/" + req.files[0].filename; 
-      fs.unlinkSync(filePath);
-      res.send(fileDetails);
-    });
-  });
+        log.info('Saved', file)
+      })
+      let filePath = "./uploads/" + req.files[0].filename
+      fs.unlinkSync(filePath)
+      res.send(fileDetails)
+    })
+  })
   
   
-app.get('/', function(req, res){ 
-    res.sendFile('./templates/index.html');
-    });
+app.get('/',(req, res) =>{ 
+    res.sendFile('./templates/index.html')
+    })
 
 
-app.listen(process.env.PORT || 8080, process.env.IP || "0.0.0.0", function(){
-  console.log("fileMetadata started");
-});
+app.listen(process.env.PORT || 8080, process.env.IP || "0.0.0.0", () =>{
+  console.log("fileMetadata started")
+})
