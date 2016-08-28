0a702f44b6e5e8f221632a6902e72418e532c860
Prince Njoku <princenjoku55@gmail.com>
2016-08-28 23:25:26 +0000

    More explicit env constants, One liner arrow functions, Uses Object Methods

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ let fileSchema = new Schema({
 let app = express()
 
 let File = mongoose.model('File', fileSchema)
-let mongouri = process.env.MONGOLAB_URI || "mongodb://localhost:27017/metadata"
+let mongouri = process.env.FILE_UPLOAD_MONGOLAB_URI || "mongodb://localhost:27017/metadata"
 mongoose.Promise = global.Promise
 mongoose.connect(mongouri)
 
@@ -30,10 +30,8 @@ app.use(multer({
 }).any())
 
 let storage = multer.diskStorage({
-  destination: (req, file, cb) =>{
-    cb(null, 'uploads/')
-  },
-  filename: (req, file, cb) =>{
+  destination (req, file, cb) { cb(null, 'uploads/') },
+  filename (req, file, cb) {
     let getFileExt = (fileName) => {
       let fileExt = fileName.split(".")
       if (fileExt.length === 1 || (fileExt[0] === "" && fileExt.length === 2)) {
@@ -51,10 +49,10 @@ let multerUpload = multer({
 let uploadFile = multerUpload.single('userFile')
 
 app.post('/upload', (req, res) => {
-    uploadFile(req, res, function(err) {
-      if (err) {
-        log.error(err)
-      }
+    uploadFile(req, res, (err) => {
+    if (err){ 
+     log.error(err)
+    }
       let fileDetails = {
         name: req.files[0].originalname,
         size: req.files[0].size,
@@ -76,11 +74,7 @@ app.post('/upload', (req, res) => {
   })
   
   
-app.get('/',(req, res) =>{ 
-    res.sendFile('./templates/index.html')
-    })
+app.get('/',(req, res) => res.sendFile('./templates/index.html'))
 
 
-app.listen(process.env.PORT || 8080, process.env.IP || "0.0.0.0", () =>{
-  console.log("fileMetadata started")
-})
+app.listen(process.env.FILE_UPLOAD_SERVER_PORT || 8080, process.env.FILE_UPLOAD_SERVER_IP || "0.0.0.0", () => console.log("fileMetadata started"))
